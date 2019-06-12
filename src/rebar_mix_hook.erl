ccbf38f1c3bf2b86b8ecd107feccd9309ae8c8e7
Mattias Hansson <mattias.hansson@axis.com>
2019-06-12 11:02:15 +0200

    rebar_mix_hook: Make use of the namespace macro
    Reducing minor redundancy by making use of the already defined namespace
    macro in the provider specification.

--- src/rebar_mix_hook.erl
+++ src/rebar_mix_hook.erl
@@ -18,7 +18,7 @@ init(State) ->
     State1 = rebar_state:add_provider(State,
                                       providers:create([{name, ?PROVIDER},
                                                         {module, ?MODULE},
-                                                        {namespace, mix},
+                                                        {namespace, ?NAMESPACE},
                                                         {bare, true},
                                                         {deps, ?DEPS},
                                                         {example, "rebar3 mix consolidate_protocols"},
